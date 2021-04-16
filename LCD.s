1058a592bc1346940ee87120f3c6f8c7222f68a8
chloetang28 <chloeatang@utexas.edu>
2021-04-15 23:17:31 -0500

    Update LCD.s

--- LCD.s
+++ LCD.s
@@ -24,23 +24,90 @@ I2C3_MDR_R  EQU 0x40023008
 I2C_Send2
 ;; --UUU-- 
 ; 1) wait while I2C is busy, wait for I2C3_MCS_R bit 0 to be 0
+	PUSH{R4, R5}
+start
+	LDR R3, =I2C3_MCS_R
+	LDRB R4, [R3]
+	AND R4, R4, #0x01 ; get bit 0 
+	CMP R4, #0
+	BNE start
 ; 2) write slave address to I2C3_MSA_R, 
 ;     MSA bits7-1 is slave address
 ;     MSA bit 0 is 0 for send data
+again
+	LSL R0, #1 ; to get to bits 7-1
+	MOV R4, R0
+	AND R4, R4, #0x01
+	CMP R4, #0
+	BNE again ; makes sure bit 0 of R0 = 0
+	LDR R3, =I2C3_MSA_R
+	STRB R0, [R3] 
 ; 3) write first data to I2C3_MDR_R
+	LDR R3, =I2C3_MDR_R
+	STRB R1, [R3]
 ; 4) write 0x03 to I2C3_MCS_R,  send no stop, generate start, enable
 ; add 4 NOPs to wait for I2C to start transmitting
+	MOV R3, #0x03
+	LDR R4, =I2C3_MCS_R
+	STRB R3, [R4]
+	NOP
+	NOP
+	NOP
+	NOP
 ; 5) wait while I2C is busy, wait for I2C3_MCS_R bit 0 to be 0
+wait
+	LDR R3, =I2C3_MCS_R
+	LDRB R4, [R3]
+	AND R4, R4, #0x01 ; get bit 0 
+	CMP R4, #0
+	BNE wait	
 ; 6) check for errors, if any bits 3,2,1 I2C3_MCS_R is high 
 ;    a) if error set I2C3_MCS_R to 0x04 to send stop 
 ;    b) if error return R0 equal to bits 3,2,1 of I2C3_MCS_R, error bits
+	LDR R3, =I2C3_MCS_R
+	LDRB R4, [R3]
+	LSR R4, #1 
+	AND R4, R4, #0x07
+	CMP R4, #0
+	BNE error
 ; 7) write second data to I2C3_MDR_R
+	LDR R3, =I2C3_MDR_R
+	STRB R2, [R3]	
 ; 8) write 0x05 to I2C3_MCS_R, send stop, no start, enable
 ; add 4 NOPs to wait for I2C to start transmitting
+	MOV R3, #0x05
+	LDR R4, =I2C3_MCS_R
+	STRB R3, [R4]
+	NOP
+	NOP
+	NOP
+	NOP
 ; 9) wait while I2C is busy, wait for I2C3_MCS_R bit 0 to be 0
+wait2
+	LDR R3, =I2C3_MCS_R
+	LDRB R4, [R3]
+	AND R4, R4, #0x01 ; get bit 0 
+	CMP R4, #0
+	BNE wait2			
 ; 10) return R0 equal to bits 3,2,1 of I2C3_MCS_R, error bits
 ;     will be 0 is no error
-   
+	LDR R3, =I2C3_MCS_R
+	LDRB R4, [R3]
+	LSR R4, #1 
+	AND R4, R4, #0x07
+	CMP R4, #0
+	BNE error
+	MOV R0, #0
+	B done
+
+error
+	MOV R0, R4 ; R0 will return with error bits
+	LSL R0, #1 ; need this??
+	MOV R4, #0x04
+	LDR R3, =I2C3_MCS_R
+	STRB R4, [R3]
+done
+	POP{R4, R5}	
 
     BX  LR                          ;   return
 
